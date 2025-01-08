5ec184ae46b01ac93af6364ca209995e5c54a16e
sofaspawn <h.m1nus.work@gmail.com>
2025-01-08 23:25:13 +0530

    hash table implementation

--- src/hash_table.c
+++ src/hash_table.c
@@ -19,3 +19,24 @@ ht_hash_table* ht_new(){
 
     return ht;
 }
+
+static void ht_del_item(ht_item* i){
+    free(i->key);
+    free(i->value);
+    free(i);
+}
+
+void ht_del_hash_table(ht_hash_table* ht){
+    for (int i = 0; i<ht->size; i++){
+        ht_item * item = ht->items[i];
+        if (item != NULL){
+            ht_del_item(item);
+        }
+    }
+    free(ht->items);
+    free(ht);
+}
+
+static int ht_hash(const char* s, const int a, const int m){
+    return 0;
+}
